5b3c8d67fd29a81c398b978df3999f2ac485dcd5
ShathaMansourr <shatha.mansour@projectsd.eu>
2026-05-03 02:19:41 +0000

    added StringEqualsConverter

---
+++ StarterApp/Converters/StringEqualsConverter.cs
@@ -0,0 +1,12 @@
+using System.Globalization;
+
+namespace StarterApp.Converters;
+
+public class StringEqualsConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => value?.ToString() == parameter?.ToString();
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
